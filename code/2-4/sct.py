c48317bc8ba9b402e2835fdd9bec4aeda248f902
Sung Kim <hunkim@gmail.com>
2020-05-09 21:39:19 +0900

    added

---
+++ code/2-4/sct.py
@@ -0,0 +1,5 @@
+test_object("c1")
+test_object("c2")
+
+test_function("print")
+success_msg("Great job!")
---
+++ code/2-4/solution.py
@@ -0,0 +1,11 @@
+pi = 3.14
+
+r1 = 10
+r2 = 20
+
+c1 = pi * r1 * r1
+c2 = pi * r2 * r2
+
+diff = c2 - c1
+print(diff)
+
