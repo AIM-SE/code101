73e645bb4fe30c71ab510618ff2f0a88caab6294
Sung Kim <hunkim@gmail.com>
2020-05-09 20:35:36 +0900

    added

---
+++ code/2-1/sample.py
@@ -0,0 +1,11 @@
+my_money = 100
+my_spent = 50
+chulsu_rent = 10
+
+rest = my_money - my_spent - chulsu_rent
+
+total_rest = rest * 12
+total_chulsu_rent = chulsu_rent * 12
+
+print (total_rest)
+print (total_chulsu_rent)
